0d3ff7bb5d85023bb549ba7c1187af3b8746839b
ajibukunoluwa <ibukunajmoti@gmail.com>
2021-12-31 14:20:41 +0100

    added contract for enum classes

---
+++ src/Contracts/IsEnumContract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tarzancodes\RolesAndPermissions\Contracts;
+
+interface IsEnumContract
+{
+    /**
+     * Get all values
+     *
+     * @return array
+     */
+    public static function all(): array;
+
+}
